e3d1d5fc2b9f984d008371a10da94bc62825826b
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 17:13:51 +0100

    Exercice completé

---
+++ TLE - Exercice 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLE___Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+            Console.WriteLine(week[1]);
+            Console.WriteLine(week[4]);
+        }
+    }
+}
